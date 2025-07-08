1fa97717ddf06e2f72d989a249cc9ae8e5d57b52
MohanadHanyZain <mohanedzainelabden2024@gmail.com>
2025-07-08 15:32:04 +0300

    Add files via upload

--- Js/main.js
+++ Js/main.js
@@ -10,3 +10,9 @@ window.onscroll = function(){
         header.classList.remove("active")
     }
 }
+
+let nav_links = document.getElementById("links");
+
+function open_colose_menu() {
+    nav_links.classList.toggle("active")
+}
